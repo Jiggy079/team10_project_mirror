6f309bf9f577c2ed1cd9d3408d2f84d1a82c3f16
nita-kras <nita.krasnita@gmail.com>
2023-03-06 10:51:11 +0000

    Add Basic Menu- draft 1

--- current_app/src/pages/exploration.js
+++ current_app/src/pages/exploration.js
@@ -1,4 +1,45 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+function BasicMenu() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+	  setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+	  setAnchorEl(null);
+	};
+  
+	return (
+	  <div>
+		<Button
+		  id="basic-button"
+		  aria-controls={open ? 'basic-menu' : undefined}
+		  aria-haspopup="true"
+		  aria-expanded={open ? 'true' : undefined}
+		  onClick={handleClick}
+		>
+		  Dashboard
+		</Button>
+		<Menu
+		  id="basic-menu"
+		  anchorEl={anchorEl}
+		  open={open}
+		  onClose={handleClose}
+		  MenuListProps={{
+			'aria-labelledby': 'basic-button',
+		  }}
+		>
+		  <MenuItem onClick={handleClose}>Profile</MenuItem>
+		  <MenuItem onClick={handleClose}>My account</MenuItem>
+		  <MenuItem onClick={handleClose}>Logout</MenuItem>
+		</Menu>
+	  </div>
+	);
+  }
 
 class Exploration extends React.Component {
 	constructor(props) {
@@ -7,7 +48,10 @@ class Exploration extends React.Component {
 
 	render () {
 		return (
-			<h1>Hello world</h1>
+			<div>
+				{/* menu bar */}
+				BasicMenu(); 
+			</div>
 		);
 	}
 }
