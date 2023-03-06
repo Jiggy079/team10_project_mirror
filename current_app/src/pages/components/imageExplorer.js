504c98e0787f300316baaf3ab4b4f2083caaf0c4
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2023-03-06 16:41:22 +0000

    feat: add component structure and definitions inside exploration file

---
+++ current_app/src/pages/components/imageExplorer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class ImageExplorer {
+
+}
+
+export default ImageExplorer;
--- current_app/src/pages/exploration.js
+++ current_app/src/pages/exploration.js
@@ -1,45 +1,7 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
-{/* function BasicMenu() {
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-	  setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-	  setAnchorEl(null);
-	};
-  
-	return (
-	  <div>
-		<Button
-		  id="basic-button"
-		  aria-controls={open ? 'basic-menu' : undefined}
-		  aria-haspopup="true"
-		  aria-expanded={open ? 'true' : undefined}
-		  onClick={handleClick}
-		>
-		  Filter
-		</Button>
-		<Menu
-		  id="basic-menu"
-		  anchorEl={anchorEl}
-		  open={open}
-		  onClose={handleClose}
-		  MenuListProps={{
-			'aria-labelledby': 'basic-button',
-		  }}
-		>
-		  <MenuItem onClick={handleClose}>Colour</MenuItem>
-		  <MenuItem onClick={handleClose}>Black and White</MenuItem>
-		  <MenuItem onClick={handleClose}>Greyscale</MenuItem>
-		</Menu>
-	  </div>
-	);
-  } */}
+import MenuBar from "components/menubar";
+import BasicMenu from "components/searchbar";
+import ImageExplorer from "components/imageExplorer";
 
 class Exploration extends React.Component {
 	constructor(props) {
@@ -49,9 +11,9 @@ class Exploration extends React.Component {
 	render () {
 		return (
 			<div>
-				<h1>Hello World</h1>
-				{/* menu bar */}
-				BasicMenu(); 
+				<MenuBar />
+				<BasicMenu />
+				<ImageExplorer />
 			</div>
 		);
 	}
