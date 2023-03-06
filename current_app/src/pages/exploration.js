ce93646236e0f814db8d88a74d70ff7861e27049
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2023-03-06 18:25:47 +0000

    fix: add import for exploration.css

--- current_app/src/pages/exploration.js
+++ current_app/src/pages/exploration.js
@@ -2,6 +2,7 @@ import React from "react";
 import MenuBar from "./components/menubar";
 import BasicMenu from "./components/searchbar";
 import ImageExplorer from "./components/imageExplorer";
+import "./exploration.css";
 
 class Exploration extends React.Component {
 	constructor(props) {
