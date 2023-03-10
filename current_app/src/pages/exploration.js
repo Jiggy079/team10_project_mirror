8bee6b2e6689e47ec97b9fe5dc6c0ea242b18ec9
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2023-03-10 16:25:40 +0000

    feat: add callback and year filter passing to ImageExplorer, add comments documenting usage etc

--- current_app/src/pages/exploration.js
+++ current_app/src/pages/exploration.js
@@ -8,6 +8,16 @@ import "./exploration.css";
 class Exploration extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			minYear: null,
+			maxYear: null
+		}
+		this.yearFilterCallback = this.yearFilterCallback.bind(this);
+	}
+
+	// this function is passed to YearSlider and should be called in its onChange function with the min and max year as parameters
+	yearFilterCallback(value1, value2) {
+		this.setState({minYear: value1, maxYear: value2});
 	}
 
 	render () {
@@ -15,10 +25,14 @@ class Exploration extends React.Component {
 		return (
 			<div>
 				<MenuBar />
-				{/*<CheckboxesTags/>*/}
-				<YearSlider/>
-				<ImageExplorer />
 
+				{/*<CheckboxesTags/>*/}
+				{/* The callback function can be called with the syntax props.yearFilterCallback(params) */}
+				<YearSlider callback={this.yearFilterCallback}/>
+
+				{/* Render ImageExplorer component with the min and max year of images it should display respectively */}
+				{/* These values can be accessed within ImageExplorer via its props object, e.g. props.minYear */}
+				<ImageExplorer minYear={this.state.minYear} maxYear={this.state.maxYear} />
 			</div>
 		);
 	}
